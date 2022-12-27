c59888d00b7931074a4690b1729803ab45f155f9
Sabyasachi <yoboy907@gmail.com>
2022-12-27 21:20:56 +0530

    Test working

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const PORT = 8000;
 
 const axios = require("axios");
 const cheerio = require("cheerio");
+const { json } = require("express");
 const express = require("express");
+const labs = require("./config.json");
 
 const app = express();
 
-let newvar = [];
+const url =
+  "https://www.cloudskillsboost.google/public_profiles/c99ed356-496c-492a-a2da-8f9f28713771";
 
-const url = "https://www.cloudskillsboost.google/public_profiles/c99ed356-496c-492a-a2da-8f9f28713771";
-
-getData(url).then((res) => {console.log(res)});
+getData(url).then((res) => {
+  console.log(res);
+});
 
 async function getData(url) {
-
   axios(url)
     .then((response) => {
       const html = response.data;
@@ -21,16 +23,23 @@ async function getData(url) {
       const badges = [];
       $(`.profile-badge`, html).each(function () {
         const title = $(`.ql-subhead-1`, this).text().trim().replace("\n", "");
-        const date = $(`.ql-body-2`, this).text().trim().replace("\n", "");
+        // const date = $(`.ql-body-2`, this).text().trim().replace("\n", "");
         const url = $(this).find("a").attr("href");
         badges.push({
           title,
-          date,
           url,
         });
       });
-      console.log(badges);
-    //   return badges;
+      fetch(
+        "https://raw.githubusercontent.com/Sabyasachi-Seal/QwiklabsProgressTracker/testing/config.json?token=GHSAT0AAAAAAB3KE3ZYHJ4S3LMAHMQMIBXUY5LECLA"
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          badges.forEach((b) => {
+            if(json.hasOwnProperty(b.title)) console.log(b.title)
+            // console.log(''+b.title+': "'+b.url+'"')
+          });
+        });
     })
     .catch((err) => console.log(err));
 }
