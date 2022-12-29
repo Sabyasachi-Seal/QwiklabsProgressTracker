9bb1169f2e0721e7b87c76bb3d3bb9d48c4b8c05
Sabyasachi <yoboy907@gmail.com>
2022-12-29 14:11:48 +0530

    Port fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || PORT;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.set("view engine", "pug");
