82aeecbf6d42a4638d577535d0817ee85d109677
Sabyasachi <yoboy907@gmail.com>
2022-12-29 18:26:42 +0530

    Styles

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const data = [
   "Perform Foundational Infrastructure Tasks in Google Cloud",
   "Preparing for Your Associate Cloud Engineer Journey",
   "Essential Google Cloud Infrastructure: Core Services",
-  "Logging, Monitoring and Observability in Google Cloud",
-  "Getting Started with Terraform for Google Cloud",
-  "Elastic Google Cloud Infrastructure: Scaling and Automation",
-  "Automating Infrastructure on Google Cloud with Terraform",
+  "(Optional) Logging, Monitoring and Observability in Google Cloud",
+  "(Optional) Getting Started with Terraform for Google Cloud",
+  "(Optional) Elastic Google Cloud Infrastructure: Scaling and Automation",
+  "(Optional) Automating Infrastructure on Google Cloud with Terraform"
 ];
 
 export default data;
