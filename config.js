9e9f54b49cf9f3d10484a1c6d4f97808c18421c5
Sabyasachi <yoboy907@gmail.com>
2023-09-15 09:14:48 +0530

    Edited for Study Jams

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
 const data = [
-  "Google Cloud Fundamentals: Core Infrastructure",
-  "Essential Google Cloud Infrastructure: Foundation",
-  "Essential Google Cloud Infrastructure: Core Services",
-  "Getting Started with Google Kubernetes Engine",
+  "Level 3: GenAI",
+  "Google Cloud Computing Foundations: Cloud Computing Fundamentals",
   "Create and Manage Cloud Resources",
+  "Google Cloud Computing Foundations: Infrastructure in Google Cloud",
   "Perform Foundational Infrastructure Tasks in Google Cloud",
-  "Set Up and Configure a Cloud Environment in Google Cloud",
-  "(Optional) Preparing for Your Associate Cloud Engineer Journey",
-  "(Optional) Logging, Monitoring and Observability in Google Cloud",
-  "(Optional) Getting Started with Terraform for Google Cloud",
-  "(Optional) Elastic Google Cloud Infrastructure: Scaling and Automation",
-  "(Optional) Automating Infrastructure on Google Cloud with Terraform"
+  "Google Cloud Computing Foundations: Networking & Security in Google Cloud",
+  "Build and Secure Networks in Google Cloud",
+  "Perform Foundational Data, ML, and AI Tasks in Google Cloud",
+  "Google Cloud Computing Foundations: Data, ML, and AI in Google Cloud"
 ];
 
 export default data;
